299de876b1c6157f287a05dc73a684377bcf4adf
Freddy Fallon <freddyfallon@gmail.com>
2017-02-20 16:27:08 +0000

    Adding exercise 13

---
+++ chap13/ex13b.rb
@@ -0,0 +1,3 @@
+one, two, three, four = ARGV
+
+puts "Your first is #{one}, your second is #{two}, your third is #{three}, and your fourth is #{four}."
---
+++ chap13/ex13c.rb
@@ -0,0 +1,4 @@
+one, two = ARGV
+print "Hit me with another here: "
+three = $stdin.gets.chomp
+puts "This is shorter! Number one is #{one} and number two is #{two}. But that third one you gave me is #{three}"
