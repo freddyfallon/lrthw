9ef12f00853cb9de865ac0f9157a371b7c001aaf
Freddy Fallon <freddyfallon@gmail.com>
2017-02-20 19:38:43 +0000

    Adding exercise 17

---
+++ chap17/ex17.rb
@@ -0,0 +1,5 @@
+from_file, to_file = ARGV ;in_file = open(from_file)
+indata = in_file.read
+out_file = open(to_file, 'w') ; out_file.write(indata)
+puts "Alright, it's done."
+out_file.close; in_file.close
