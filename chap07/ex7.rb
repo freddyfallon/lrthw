c26e2f380a0cbbf3c441db6c6d0778765540d6c3
Freddy Fallon <freddyfallon@gmail.com>
2017-02-20 15:15:13 +0000

    Adding exercise 7

---
+++ chap07/ex7.rb
@@ -0,0 +1,27 @@
+# prints a string to the screen with a new line
+puts "Mary had a little lamb."
+# prints a string with string interpolation
+puts "Its fleece was white as #{'snow'}."
+# prints a string to the screen, new line at end
+puts "And everywhere that Mary went."
+# prints a . 10 times
+puts "." * 10 #what'd that do?
+
+# assigns strings to 12 variables
+end1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+end10 = "g"
+end11 = "e"
+end12 = "r"
+
+# watch that print vs. puts on this line what's it do?
+print end1 + end2 + end3 + end4 + end5 + end6
+puts end7 + end8 + end9 + end10 + end11 + end12
+# prints all on one line because there is no new line after end6
