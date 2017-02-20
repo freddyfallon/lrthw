d88e385ea6cdd680c6c9d4fe5f0d1717659be018
Freddy Fallon <freddyfallon@gmail.com>
2017-02-20 10:45:10 +0000

    Adding exercise 3

---
+++ chap03/ex3.rb
@@ -0,0 +1,33 @@
+# Prints a string with a new line at the end
+puts "I will now count my chickens:"
+
+# Prints a string and then uses string interpolation to do math inside the string
+puts "Hens #{25.0 + 30.0 / 6.0}"
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# Prints another string with a new line at the end
+puts "Now I will count the eggs:"
+
+# Does math and then prints the result to the screen
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Prints another string with a new line at the end
+puts "Is it true that 3.0 + 2.0 < 5.0 - 7.0?"
+
+# Does math and then prints the result to the screen
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# Prints a string and then uses string interpolation to do math inside the string
+puts "What is 3.0 + 2.0? #{3.0 + 2.0}"
+puts "What is 5.0 - 7.0? #{5.0 - 7.0}"
+
+# Prints another string with a new line at the end
+puts "Oh, that's why it is false."
+
+# Prints another string with a new line at the end
+puts "How about some more."
+
+# Prints a string and then uses string interpolation to do math inside the string
+puts "Is it greater? #{5.0 > -2.0}"
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+puts "Is it less or equal? #{5.0 <= -2.0}"
---
+++ chap03/ex3b.rb
@@ -0,0 +1,6 @@
+puts "How do you convert centimetres into inches?"
+puts "You multiply the number of centimetres by 0.393701"
+
+puts "Like this:"
+puts "1 centimeter is equal to #{1 * 0.393701} inches."
+puts "23 centimeters is equal to #{23 * 0.393701} inches."
