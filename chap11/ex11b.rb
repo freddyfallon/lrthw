d0022692d5b55254eb9739fd74f43235c9430767
Freddy Fallon <freddyfallon@gmail.com>
2017-02-20 16:10:26 +0000

    Adding ex11 and ex11b

---
+++ chap11/ex11b.rb
@@ -0,0 +1,6 @@
+print "Who is your favourite wrestler? "
+wrestler = gets.chomp
+print "Who is your favourite rapper? "
+rapper = gets.chomp
+
+puts "Imagine if #{wrestler} wrestled #{rapper}. I think #{rapper} might actually win."
