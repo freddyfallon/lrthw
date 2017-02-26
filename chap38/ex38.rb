35a6b9fe4cd16fbde214fb73b2750bf15c641b6e
Freddy Fallon <freddyfallon@gmail.com>
2017-02-26 15:41:31 +0000

    Updating exercise 38

--- chap38/ex38.rb
+++ chap38/ex38.rb
@@ -2,17 +2,26 @@ ten_things = "Apples Oranges Crows Telephone Light Sugar"
 
 puts "Wait there are not 10 things in that list. Let's fix that."
 
+# Ruby looks for an object called ten_things, finds it, looks for split, finds it,
+# Calls ' ' as it's argument but then ten_things as a sub-argument
 stuff = ten_things.split(' ')
 more_stuff = ["Day", "Night", "Song", "Frisbee", "Corn", "Banana", "Girl", "Boy"]
 
 # using math to make sure there's 10 items
 
 while stuff.length != 10
+  # checks that more_stuff exists, finds it does, looks for .pop, finds it
+  # executes it (by removing last element) with more_stuff as its argument and assigns it to next_one
   next_one = more_stuff.pop
   puts "Adding #{next_one}"
-  stuff.push("next_one")
+  # checks that stuff exists, finds it does, looks for push, finds it
+  # executes it (by adding contents of next_one to end of stuff)
+  # uses next_one as first argument, then stuff as a sub-argument
+  stuff.push(next_one)
   puts "There are #{stuff.length} items now."
 end
+
+puts "There we go: #{stuff}"
 
 puts "Let's do some things with stuff."
 
