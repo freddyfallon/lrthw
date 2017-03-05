9db93f4ad842201477b319e78e4388f69e50b427
Freddy Fallon <freddyfallon@gmail.com>
2017-03-05 07:44:43 +0000

    Adding another song to exercise 40

--- chap40/ex40.rb
+++ chap40/ex40.rb
@@ -15,6 +15,12 @@ happy_bday = Song.new(["Happy birthday to you",
 bull_on_parade = Song.new(["They rally around tha family",
                       "With pockets full of shells"])
 
-happy_bday.sing_me_a_song()
+times_they_are_a_changin = Song.new(["Come gather round",
+                      "Whereever you roam"])
 
-bull_on_parade.sing_me_a_song()
+
+happy_bday.sing_me_a_song
+
+bull_on_parade.sing_me_a_song
+
+times_they_are_a_changin.sing_me_a_song
